552ed085a0429661b09078965c1717945d7e0e4e
Rajat130 <rajatlokhande1999@gmail.com>
2025-05-08 14:53:30 +0530

    adding environment factory

--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -1,38 +1,44 @@
 import axios from 'axios';
 import jwt from 'jsonwebtoken';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { EnvironmentFactory } from '../endpoint';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
 
-  const backendUrl = 'https://dev-wa-api.triggrsweb.com/auth/login/password';
-  
+  const environment = EnvironmentFactory.getEnvironment(process.env.STAGE);
+  // console.log(apiUrl);
   try {
-    const response = await axios.post(backendUrl, req.body, {
+    const response = await axios({
+      method: 'POST',
+      url: `${environment?.config?.wa?.apiUrl}/auth/login/password`,
+      data: req.body,
       headers: {
         'Content-Type': 'application/json',
       },
     });
-    
+
     if (!response.data.token) {
       throw new Error('No token received from backend');
     }
 
-    // Generate a new JWT token with the user data
-    const token = jwt.sign({
-      user: response.data.user,
-      exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60) // Token expires in 24 hours
-    }, process.env.SESS_SECRET_TOKEN);
+    const token = jwt.sign(
+      {
+        user: response.data.user,
+        exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
+      },
+      process.env.SESS_SECRET_TOKEN
+    );
 
-    // Set the token in a cookie
-    res.setHeader('Set-Cookie', `twchat=${token}; Path=/; HttpOnly; SameSite=Lax; Secure`);
-    
-    // Return the user data and token
+    res.setHeader(
+      'Set-Cookie',
+      `twchat=${token}; Path=/; HttpOnly; SameSite=Lax; Secure`
+    );
+
     res.status(200).json({
       user: response.data.user,
-      token: token
+      token: token,
     });
   } catch (error) {
     console.error('Error fetching data from backend:', error);
